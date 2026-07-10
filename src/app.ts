662cc50ee2bf65e9d4dac384984446cc60e4f159
phanithmin-source <phanith.min@allweb.com.kh>
2026-07-10 10:55:25 +0700

    Revert "Chore; cors blocking"
    This reverts commit 6a70a7f51d9fda29751c67a1fa3cb6bb9c467632.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { env } from "./config/env.js";
 
 const app = express();
 
 app.use(
   cors({
-    origin: env.FRONTEND_URL.split(","),
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
